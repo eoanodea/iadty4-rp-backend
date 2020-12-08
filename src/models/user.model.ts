9cbfd716100973bc636b6db4985d754674f6b7b7
Eoan O Dea <odeaeoan@gmail.com>
2020-12-08 20:47:04 +0100

    user model bug fixing

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,7 +6,7 @@
  * Author: Eoan O'Dea (art@psioniq.uk)
  * -----
  * File Description: Model which represents the User schema
- * Last Modified: Tuesday, 8th December 2020 8:30:23 pm
+ * Last Modified: Tuesday, 8th December 2020 8:46:46 pm
  * Modified By: Eoan O'Dea (eoan@web-space.design>)
  * -----
  * Copyright 2020 WebSpace, WebSpace
@@ -56,9 +56,9 @@ const UserSchema: Schema = new Schema({
   updated: { type: Date },
   email: {
     type: String,
-    trim: true,
+    trim: "true",
     unique: "Email already exists",
-    match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    match: ["/.+@.+..+/", "Please fill a valid email address"],
     required: "Email is required",
   },
   hashed_password: {
